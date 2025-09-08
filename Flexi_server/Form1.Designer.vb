f2d1fc626c992c87823d6c3e8ad13076aa172df7
farukjnu1@gmail.com <farukjnu1@gmail.com>
2025-09-08 21:56:05 +0800

    Add project files.

---
+++ Flexi_server/Form1.Designer.vb
@@ -0,0 +1,165 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(108, 1)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox1.TabIndex = 0
+        Me.TextBox1.Text = "COM6"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(239, -1)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(171, 32)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "CONNECT"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(372, 147)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(184, 49)
+        Me.Button2.TabIndex = 5
+        Me.Button2.Text = "Send"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(50, 4)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(44, 13)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "PORT#"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(289, 101)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(0, 20)
+        Me.Label5.TabIndex = 10
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(641, 147)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(193, 49)
+        Me.Button3.TabIndex = 11
+        Me.Button3.Text = "Button3"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(26, 290)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(39, 13)
+        Me.Label6.TabIndex = 12
+        Me.Label6.Text = "Label6"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(26, 224)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(39, 13)
+        Me.Label7.TabIndex = 13
+        Me.Label7.Text = "Label7"
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 3000
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(26, 256)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(39, 13)
+        Me.Label8.TabIndex = 15
+        Me.Label8.Text = "Label8"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(444, 18)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(61, 13)
+        Me.Label2.TabIndex = 16
+        Me.Label2.Text = "11-02-2015"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(857, 499)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents Label8 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+
+End Class
---
+++ Flexi_server/Form1.vb
@@ -0,0 +1,321 @@
+﻿Imports System.Data.SqlClient
+Imports System.IO.Ports
+Imports System.Management
+Imports System.Threading
+Imports System.Text.RegularExpressions
+
+Public Class Form1
+    Dim revdata As String = ""
+    Dim b As Integer
+    Dim con As New SqlConnection
+    Dim cmd As New SqlCommand
+    Dim rd As SqlDataReader
+    Dim loopbreak As Integer
+
+
+    Public Event DataReceived As IO.Ports.SerialDataReceivedEventHandler
+
+    Dim WithEvents Serialport1 As New IO.Ports.SerialPort
+
+    
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        con.ConnectionString = "Server=localhost;Database=Flexiload;User Id=sa;Password=123;"
+        cmd.Connection = con
+        Serialport1.PortName = TextBox1.Text
+        Serialport1.BaudRate = 9600
+        Serialport1.Parity = IO.Ports.Parity.None
+        Serialport1.DataBits = 8
+        Serialport1.StopBits = IO.Ports.StopBits.One
+        Serialport1.Handshake = IO.Ports.Handshake.None
+        'Serialport1.RtsEnable = True
+
+        Serialport1.Open()
+        Timer1.Start()
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim flagid As Int32
+        Dim Phone As String
+        Dim Amount As Int32
+        Dim rid As Int32
+
+        
+        con.Open()
+
+        cmd.CommandText = "SELECT * FROM [Flexiload].[dbo].[Pending] Where Status=0"
+        rd = cmd.ExecuteReader
+        rd.Read()
+        If rd.HasRows Then
+            'rd.NextResult()
+            'abc = rd.GetString(0)
+            flagid = rd.GetInt32(3)
+            Phone = rd.GetString(1)
+            Amount = rd.GetInt32(2)
+            rid = rd.GetInt32(0)
+            con.Close()
+
+            If flagid = 0 Then
+                Serialport1.Write("AT+STGI=0" & vbCrLf)
+                Threading.Thread.Sleep(300)
+                Serialport1.Write("AT+STGR=0,1,128" & vbCrLf)
+                Threading.Thread.Sleep(1500)
+                Serialport1.Write("AT+STGI=6" & vbCrLf)
+                Threading.Thread.Sleep(300)
+                Serialport1.Write("AT+STGR=6,1,1" & vbCrLf)
+                Threading.Thread.Sleep(1500)
+                Serialport1.Write("AT+STGI=3" & vbCrLf)
+                Threading.Thread.Sleep(300)
+                Serialport1.Write("AT+STGR=3,1" & vbCr)
+                Threading.Thread.Sleep(300)
+                Serialport1.Write(Phone & Chr(26)) 'Cnumber
+                Threading.Thread.Sleep(1500)
+                Serialport1.Write("AT+STGI=3" & vbCrLf)
+                Threading.Thread.Sleep(300)
+                Serialport1.Write("AT+STGR=3,1" & vbCr)
+                Threading.Thread.Sleep(300)
+                Serialport1.Write(Phone & Chr(26)) 'Cnumber2
+                Threading.Thread.Sleep(1500)
+                Serialport1.Write("AT+STGI=3" & vbCrLf)
+                Threading.Thread.Sleep(1500)
+                Serialport1.Write("AT+STGR=3,1" & vbCr)
+                Threading.Thread.Sleep(300)
+                Serialport1.Write(Amount & Chr(26))  'amount
+                Threading.Thread.Sleep(1500)
+                Serialport1.Write("AT+STGI=3" & vbCrLf)
+                Threading.Thread.Sleep(1500)
+                Serialport1.Write("AT+STGR=3,1" & vbCr)     'amount2
+                Threading.Thread.Sleep(300)
+                Serialport1.Write(Amount & Chr(26))
+                Threading.Thread.Sleep(1500)
+                Serialport1.Write("AT+STGI=3" & vbCrLf)
+                Threading.Thread.Sleep(1500)
+                Serialport1.Write("AT+STGR=3,1" & vbCr)
+                Threading.Thread.Sleep(300)
+                Serialport1.Write("2580" & Chr(26) & vbCrLf) 'pin
+                Threading.Thread.Sleep(1500)
+                Serialport1.Write("AT+STGI=1" & vbCrLf)
+                Threading.Thread.Sleep(1500)
+                Serialport1.Write("AT+STGR=1,1" & vbCrLf)
+                Threading.Thread.Sleep(1500)
+                Serialport1.Write("AT+STGI=9" & vbCrLf)
+                Threading.Thread.Sleep(1500)
+                'loopbreak = 1
+                
+
+                con.Open()
+
+                cmd.CommandText = "UPDATE  [Flexiload].[dbo].[Pending] SET Status=1 WHere ID=" & rid
+                rd = cmd.ExecuteReader
+                con.Close()
+
+                Label8.Text = ("FlexiLoaD Done")
+                
+            End If
+        Else
+            con.Close()
+
+        End If
+
+    End Sub
+
+    Private Sub Serialport1_DataReceived(sender As Object, e As SerialDataReceivedEventArgs) Handles Serialport1.DataReceived
+        Dim datain As String = ""
+        Dim numbytes As Integer = Serialport1.BytesToRead
+        For i As Integer = 1 To numbytes
+            datain &= Chr(Serialport1.ReadChar)
+        Next
+        test(datain)
+
+    End Sub
+
+    Private Sub test(ByVal indata As String)
+        revdata &= indata
+        'revdata.Substring(revdata.Length, )
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        '  If revdata.ToString.Contains("+CMTI") Then
+        revdata = ""
+        Serialport1.Write("AT+CMGF=1" & vbCrLf)
+        Threading.Thread.Sleep(100)
+
+        'Serialport1.Write("AT+CPMS=""ME""" & vbCrLf)
+        'Threading.Thread.Sleep(100)
+
+        Serialport1.Write("AT+CMGL=""ALL""" & vbCrLf)
+        Threading.Thread.Sleep(4000)
+        ReadMsg()
+        'End If
+    End Sub
+
+    Private Sub ReadMsg()
+        Dim fmns As Integer
+
+        Dim lineoftext As String
+        Dim i As Integer
+        Dim arytextfile() As String
+
+        lineoftext = revdata.ToString
+        'revdata = ""
+        arytextfile = Split(lineoftext, "+CMGL", , CompareMethod.Text)
+        Dim phonen As String
+        Dim messageb As String
+        Dim datetimee As String
+        Dim messagebody As String
+        Dim mphone As String
+        Dim mindex As Integer
+
+
+
+        For i = 2 To UBound(arytextfile)
+            Dim input As String = arytextfile(i)
+            Dim result() As String
+            Dim pattern As String = "(: )|(,"")|("","")|("",,"")|("","")|("",:"")|("" "")"
+            result = Regex.Split(input, pattern)
+            phonen = result(6)
+            messageb = result(8)
+            datetimee = Mid(messageb, 1, 17)
+            mindex = result(2)
+            Serialport1.Write("AT+CMGD=" & mindex & vbCrLf)
+            Threading.Thread.Sleep(100)
+            Dim sss As Integer
+
+
+            messagebody = Mid(messageb, 22, messageb.Length - 22)
+            sss = InStr(1, messagebody, vbCrLf + vbCrLf)
+            If (sss > 0) Then
+                messagebody = Mid(messagebody, 1, sss)
+                Dim a As String
+
+                con.Open()
+                a = "INSERT INTO Flexiload.dbo.Receive_sms VALUES (" & "'" & phonen & "'" & "," & "'" & messagebody & "'" & "," & "'" & DateTime.Now().ToString("yyyy-MM-dd HH:mm:ss") & "'" & ")"
+                cmd.CommandText = a
+                rd = cmd.ExecuteReader
+                con.Close()
+
+            End If
+            Dim success As Integer
+            Dim tid As Integer
+            Dim tid1 As String
+            Dim sorry As Integer
+
+
+            Label7.Text = messagebody
+            ' mphone = Mid(messagebody, 2, TextBox2.Text.Length)
+            fmns = InStr(1, messagebody, " 17")
+            mphone = Mid(messagebody, fmns + 1, 10)
+            success = InStr(1, messagebody, "successful")
+            sorry = InStr(1, messagebody, "Sorry")
+            tid = InStr(1, messagebody, "transaction ID ")
+            tid1 = Mid(messagebody, tid + 14, 18)
+            If fmns > 0 And success > 0 Then
+                con.Open()
+                '     'INSERT INTO [AkashSMS].dbo.SendSMS VALUES ('1','+171154021','hello','successs','2014-01-01 9:00:00')
+                Dim stest As String
+                Dim flagid As Int32
+                Dim Phone As String
+                Dim Amount As Int32
+                Dim rid As Integer
+                Dim rf As String
+
+                Dim dt As String = DateTime.Now().ToString("yyyy-MM-dd HH:mm:ss")
+
+
+
+                stest = "SELECT * FROM [Flexiload].[dbo].[Pending] where Status=1 and Phone_Number LIKE " + "'" + "%" + mphone + "%" + "'"
+                cmd.CommandText = stest
+                rd = cmd.ExecuteReader
+                rd.Read()
+
+                If rd.HasRows Then
+
+
+                    'rd.NextResult()
+                    'abc = rd.GetString(0)
+                    flagid = rd.GetInt32(3)
+                    Phone = rd.GetString(1)
+                    Amount = rd.GetInt32(2)
+                    rf = rd.GetString(4)
+                    rid = rd.GetInt32(0)
+                    con.Close()
+                    con.Open()
+
+                    cmd.CommandText = "DELETE FROM [Flexiload].[dbo].[Pending] where ID=" & rid
+                    rd = cmd.ExecuteReader
+                    con.Close()
+                    con.Open()
+
+                    cmd.CommandText = "INSERT INTO [Flexiload].[dbo].[Successfull] (Phone_Number,Amount,Status,Request_From,Date_Time,Transaction_ID,FlagId) VALUES ('" & Phone & "','" & Amount & "','" & "SUCCESSFULL" & "','" & rf & "','" & dt & "','" & tid1 & "','" & flagid & "')"
+
+                    rd = cmd.ExecuteReader
+                    con.Close()
+                
+                End If
+            End If
+            If fmns > 0 And sorry > 0 Then
+                Dim stest As String
+                Dim flagid As Int32
+                Dim Phone As String
+                Dim Amount As Int32
+                Dim rid As Integer
+                Dim rf As String
+
+                Dim dt As String = DateTime.Now().ToString("yyyy-MM-dd HH:mm:ss")
+
+
+
+                con.Open()
+                stest = "SELECT * FROM [Flexiload].[dbo].[Pending] where Status=1 and Phone_Number LIKE " + "'" + "%" + mphone + "%" + "'"
+                cmd.CommandText = stest
+                rd = cmd.ExecuteReader
+                rd.Read()
+                If rd.HasRows Then
+                    rid = rd.GetInt32(0)
+                    flagid = rd.GetInt32(3)
+                    Phone = rd.GetString(1)
+                    Amount = rd.GetInt32(2)
+                    rf = rd.GetString(4)
+
+                    con.Close()
+                    con.Open()
+                    cmd.CommandText = "DELETE FROM [Flexiload].[dbo].[Pending] where ID=" & rid
+                    rd = cmd.ExecuteReader
+                    con.Close()
+                    con.Open()
+
+                    cmd.CommandText = "INSERT INTO [Flexiload].[dbo].[Successfull] (Phone_Number,Amount,Status,Request_From,Date_Time,Transaction_ID,FlagId) VALUES ('" & Phone & "','" & Amount & "','" & "REJECTED" & "','" & rf & "','" & dt & "','" & "NOT AVAILABLE" & "','" & flagid & "')"
+
+                    rd = cmd.ExecuteReader
+                    con.Close()
+                End If
+            Else
+                con.Close()
+
+
+            End If
+            b = b + 1
+            Label6.Text = Str(b)
+
+
+        Next
+        '   Serialport1.Write("AT+CMGD=1,1" & vbCrLf)
+        '         Threading.Thread.Sleep(100)
+
+
+    End Sub
+
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Timer1.Stop()
+        Button2.PerformClick()
+        Threading.Thread.Sleep(200)
+        Button3.PerformClick()
+        Threading.Thread.Sleep(200)
+
+
+        Timer1.Start()
+
+    End Sub
+
+End Class
